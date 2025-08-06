f54819cffa1990ad625891bcc10a4ecb230dd27b
Govind Kumar <govindkumar26112001@gmail.com>
2025-08-06 07:27:17 +0530

    chore: update eslint config and codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+   // Override specific rules
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
+    }
+  }
+  
 ];
 
 export default eslintConfig;
